811cb2a68cb809f5350f6c931eed6d6c3f587c3e
Ryan Rittner <rrittner@purdue.edu>
2025-08-21 12:56:54 -0700

    User manual grading endpoint functional

--- src/user_grade_email_lambda/main.py
+++ src/user_grade_email_lambda/main.py
@@ -1,2 +1,72 @@
+import base64
+import json
+import boto3
+from datetime import datetime
+from botocore.exceptions import ClientError
+from utils import score_function
+
+dynamodb = boto3.resource("dynamodb")
+GRADED_TABLE = "UserGradedEmails"  # Can be changed to UserProfiles
+
 def lambda_handler(event, context):
-    pass
+    try:
+        body = event.get("body", {})
+        if isinstance(body, str):
+            if event.get("isBase64Encoded"):
+                body = base64.b64decode(body).decode("utf-8")
+            body = json.loads(body)
+
+        user_email = body.get("user_email")
+        email_id = body.get("email_id")
+        email_body = body.get("body")
+        email_date = body.get("email_date")
+        current_date = body.get("current_date")
+        subject = body.get("subject")
+        sender = body.get("sender")
+        importance = body.get("importance")
+        urgency = body.get("urgency")
+        justification = body.get("justification")
+        score = score_function(importance, urgency)
+
+
+
+        if not user_email or not email_id or not email_date or not current_date or not sender or not importance or not urgency or not justification:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"error": "Missing required one or more required fields: user_email, email_id, email_date, current_date, email_date, sender, importance, urgency, justification"})
+            }
+
+        table = dynamodb.Table(GRADED_TABLE)
+
+        new_item = {
+            "user_email": user_email,
+            "email_id": email_id,
+            "email_date": email_date,
+            "current_date": current_date,
+            "body": email_body,
+            "subject": subject,
+            "sender": sender,
+            "importance": importance,
+            "urgency": urgency,
+            "justification": justification,
+            "score": score
+        }
+
+        table.put_item(TableName=GRADED_TABLE, Item=new_item)
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "Email grade stored"})
+        }
+
+    except ClientError as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
---
+++ src/user_grade_email_lambda/utils.py
@@ -0,0 +1,2 @@
+def score_function(importance: int, justification: int) -> int:
+    return importance + justification
